ed67a4bee41f4eb597d9493e613e4646be8cd0ba
artwebs <artwebs@126.com>
2013-10-12 00:18:00 +0800

    upate

---
+++ src/test.cpp
@@ -0,0 +1,22 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2013-10-11
+ *      Author: hongbinliu
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+char* test::printmethod()
+{
+	return "test printmethod";
+}
+
+test::~test() {
+	// TODO Auto-generated destructor stub
+}
+
